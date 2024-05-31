aeb24ba231ef921ef275f38536d82019bdbb5220
cadusaboya <carlosepsaboya@gmail.com>
2024-05-31 19:45:24 -0400

    Using auth URL

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function resetPassword() {
     const uidb64 = urlParams.get('uidb64');
     const token = urlParams.get('token');
     const password = document.getElementById('newPassword').value;
-    const API_URL = 'http://factor-backend-1480867072.sa-east-1.elb.amazonaws.com:8000';
+    const API_URL = 'https://api.factorpa.xyz/';
 
     
     const response = await fetch(`${API_URL}/accounts/reset-password/${uidb64}/${token}/`, {
