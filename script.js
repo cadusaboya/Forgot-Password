5d25a5baca7ab33ca828305c1aef7de8f16104a0
cadusaboya <carlosepsaboya@gmail.com>
2024-05-31 17:08:06 -0400

    Moved to test url

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function resetPassword() {
     const uidb64 = urlParams.get('uidb64');
     const token = urlParams.get('token');
     const password = document.getElementById('newPassword').value;
-    const API_URL = 'http://factor-backend-1480867072.sa-east-1.elb.amazonaws.com:8000';
+    const API_URL = 'https://factor-cadusaboya.loca.lt';
 
     
     const response = await fetch(`${API_URL}/accounts/reset-password/${uidb64}/${token}/`, {
