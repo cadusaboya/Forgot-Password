a25aba7a24d87c106a2d56d1fd4204c717ec0743
cadusaboya <carlosepsaboya@gmail.com>
2024-05-31 16:25:50 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+async function resetPassword() {
+    const url = window.location.href;
+    const segments = url.split('/');
+    const uidb64 = segments[segments.length - 2];
+    const token = segments[segments.length - 1];
+    const password = document.getElementById('newPassword').value;
+    
+    const response = await fetch(`https://your-django-backend-url/reset-password/${uidb64}/${token}/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ password: password }),
+    });
+
+    const data = await response.json();
+    const messageElement = document.getElementById('message');
+    if (data.message) {
+        messageElement.innerText = data.message;
+        messageElement.style.color = 'green';
+    } else if (data.error) {
+        messageElement.innerText = data.error;
+        messageElement.style.color = 'red';
+    }
+}
